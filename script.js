14b754db20abf266bc4aef76aad4469a64c29f26
Heggy Castaneda <Heggy@heggy-mbp.attlocal.net>
2020-07-09 17:47:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function marinaraSauce(cookName) {
+  let two = 2;
+  let oneFourth = "1/4";
+
+  console.log(`
+  Hey, ${cookName}! This is my marinara sauce recipe!
+
+  - Heat a medium-large saucepan over medium heat.
+  - Add ${two} tspns of oil and 5 carlic cloves, cook until golden, about ${two} minutes
+  - Add 1/4 cup water, ${two} cans of crushed tomates, and salt and season with black pepper to taste.
+  - Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes
+  - Stir in ${oneFourth} cup roughly chopped fresh basil, salt and pepper as needed.
+  `);
+}
+
+marinaraSauce("Heggy");
